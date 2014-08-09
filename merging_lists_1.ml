053bcbe2367455c043e67997569613f7cea0304e
Goran Peretin <goran.peretin@gmail.com>
2014-08-09 11:15:54 +0100

    Add merge_i function

---
+++ merging_lists_1.ml
@@ -0,0 +1,11 @@
+(* Merging two lists exercise from Developing applications with OCaml
+ * book, Chapter 2.
+ *)
+
+(* Take 2 sorted lists and return merged list, also sorted *)
+let rec merge_i xs ys =
+    match xs, ys with
+    | [], _ -> ys
+    | _, [] -> xs
+    | hx::txs, hy::tys ->
+        if hx < hy then hx::merge_i txs ys else hy::merge_i xs tys
