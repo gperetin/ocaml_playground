b251e740251d69c887a0bdd62836ade515e0508f
Goran Peretin <goran.peretin@gmail.com>
2014-08-09 11:35:45 +0100

    Add generic merge function

--- merging_lists_1.ml
+++ merging_lists_1.ml
@@ -9,3 +9,11 @@ let rec merge_i xs ys =
     | _, [] -> xs
     | hx::txs, hy::tys ->
         if hx < hy then hx::merge_i txs ys else hy::merge_i xs tys
+
+
+let rec merge cmp xs ys =
+    match xs, ys with
+    | [], _ -> ys
+    | _, [] -> xs
+    | hx::txs, hy::tys ->
+        if cmp xs ys then hx::merge cmp txs ys else hy::merge cmp xs tys
